48a741f0a2998f544d8652fb16ab243126b80270
Lily Saadoun <sawsan.n.saadoun@vanderbilt.edu>
2024-08-08 08:53:00 -0500

    implement stripe

---
+++ components/shared/Pagination.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type PaginationProps = {
+  page: number | string;
+  totalPages: number;
+  urlParamName?: string;
+};
+
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
