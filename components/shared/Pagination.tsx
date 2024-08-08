ad005d5bf78ec95a8ed0a4fd236e31702bfd9208
Lily Saadoun <sawsan.n.saadoun@vanderbilt.edu>
2024-08-08 10:44:45 -0500

    finish the app

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Button } from '../ui/button';
+import { formUrlQuery } from '@/lib/utils';
 
 type PaginationProps = {
   page: number | string;
@@ -7,7 +11,43 @@ type PaginationProps = {
 };
 
 const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
-  return <div>Pagination</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('prev')}
+        disabled={Number(page) <= 1}
+      >
+        Previous
+      </Button>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('next')}
+        disabled={Number(page) >= totalPages}
+      >
+        Next
+      </Button>
+    </div>
+  );
 };
 
 export default Pagination;
